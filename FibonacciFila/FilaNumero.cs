52e8a1023554936c8ddc43030dd36c3d0b51036d
Edenilson <edenilsonju10@gmail.com>
2024-05-03 18:59:17 -0300

    Add project files.

---
+++ FibonacciFila/FilaNumero.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FibonacciFila
+{
+    internal class FilaNumero
+    {
+        Numero? head;
+        Numero? tail;
+        int tamanho;
+
+        public FilaNumero()
+        {
+            this.head = null;
+            this.tail = null;
+        }
+
+        public void Enqueue(Numero aux)
+        {
+            if (IsEmpty())
+            {
+                head = aux;
+                tail = aux;
+            }
+            else
+            {
+                tail.SetProximo(aux);
+                tail = aux;
+            }
+            tamanho++;
+        }
+
+        public Numero? Dequeue()
+        {
+            if (!IsEmpty())
+            {
+                Numero aux = head;
+
+                if (head == tail)
+                    head = tail = null;
+                else
+                    head = head.GetProximo();
+
+                tamanho--;
+
+                return aux;
+            }
+            return null;
+        }
+
+        public int GetTamanho()
+        {
+            return tamanho;
+        }
+
+        public void Print()
+        {
+
+            if (IsEmpty())
+            {
+                Console.WriteLine("-->Fila vazia!");
+            }
+            else
+            {
+                Numero aux = head;
+
+                while (aux != null)
+                {
+                    Console.WriteLine($"--> {aux}");
+                    aux = aux.GetProximo();
+                }
+            }
+        }
+
+        public bool IsEmpty()
+        {
+            return head == null && tail == null;
+        }
+    }
+}
---
+++ FibonacciFila/Program.cs
@@ -0,0 +1,49 @@
+﻿namespace FibonacciFila
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Clear();
+            Console.WriteLine("====================");
+            Console.WriteLine(">>> Fibonacci <<<");
+
+            bool resultadoTry;
+            int tamanho, temp = 0, anterior = 0, atual = 1;
+            FilaNumero fila = new FilaNumero();
+
+            do
+            {
+                Console.WriteLine("Digite a quantidade de numeros da sequencia:");
+                Console.Write("R: ");
+
+                resultadoTry = int.TryParse(Console.ReadLine(), out tamanho);
+
+            } while (!resultadoTry);
+
+            if (tamanho <= 0)
+            {
+                Console.WriteLine("Numero incorreto!");
+            }
+            else
+            {
+                if (tamanho >= 1)
+                    fila.Enqueue(new Numero(anterior));
+                if (tamanho >= 2)
+                    fila.Enqueue(new Numero(atual));
+                for (int i = 2; i < tamanho; i++)
+                {
+                    temp = atual + anterior;
+                    anterior = atual;
+                    atual = temp;
+
+                    fila.Enqueue(new Numero(atual));
+                }
+
+                Console.WriteLine("Sequencia fibonacci: ");
+                fila.Print();
+            }
+            Console.ReadKey();
+        }
+    }
+}
